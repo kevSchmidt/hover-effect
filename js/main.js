d0717f6059db2870ab784947367a8172c6670458
Kevin <kev.saintigny.schmidt@gmail.com>
2020-12-10 09:33:56 +0100

    created repo

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector(".container"),
+    intensity1: 1,
+    intensity2: 1,
+    speedIn: 1,
+    speedOut: 1,
+    image1: "../img/Img1.jpg",
+    image2: "../img/Img2.jpg",
+    displacementImage: "../img/displacement4.jpg",
+});
